fa3180da48ea35f74ef0c3562eaed472fc9962c4
John Hill <john.m.hill91@gmail.com>
2019-04-11 04:13:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,8 @@ emotHash['get_emoticon']={}
 emoticons.each do |meaning, emoticon|
   english=emoticon[0]
   japanese=emoticon[1]
-  emotHash['get_meaning'][japanese]=
+  emotHash['get_meaning'][japanese]=meaning
+  emotHash["get_emoticon"][english]=
 end
 
 def get_japanese_emoticon
